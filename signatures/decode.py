44e8336455140b23c810052b778e4a193dbdfc3a
cccs-kevin <kevin.hardy-cooper@cyber.gc.ca>
2023-03-22 15:46:46 +0000

    Adding signature for nested atob calls

--- signatures/decode.py
+++ signatures/decode.py
@@ -72,3 +72,17 @@ class CryptoJSObfuscation(Signature):
 
     def process_output(self, output):
         self.check_indicators_in_list(output)
+
+
+class NestedAtoB(Signature):
+    def __init__(self):
+        super().__init__(
+            heuristic_id=3,
+            name="nested_atob",
+            description="JavaScript uses nested atob() calls for decoding",
+            indicators=["atob(atob("],
+            severity=0
+        )
+
+    def process_output(self, output):
+        self.check_indicators_in_list(output)
