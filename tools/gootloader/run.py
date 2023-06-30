d97b9a4ca8772bac00c63862957ecd56b21f3b2e
cccs-kevin <kevin.hardy-cooper@cyber.gc.ca>
2023-06-30 15:03:35 +0000

    Wrap back up attempt of Gootloader decoding

--- tools/gootloader/run.py
+++ tools/gootloader/run.py
@@ -19,10 +19,13 @@ def run(js_file_path: str, unsafe_uris: bool = False, payload_path: str = None, 
     except Exception as e:
         log(f"Falling back on GootLoaderAutoJsDecode due to {e}")
         """Fallback on the unmodified script"""
-        third_generation, urls, code = gootDecode(js_file_path, unsafe_uris, payload_path, stage2_path, log)
-        if third_generation:
-            print("calling again")
-            _, urls, code = gootDecode(stage2_path, unsafe_uris, payload_path, stage2_path, log)
+        try:
+            third_generation, urls, code = gootDecode(js_file_path, unsafe_uris, payload_path, stage2_path, log)
+            if third_generation:
+                print("calling again")
+                _, urls, code = gootDecode(stage2_path, unsafe_uris, payload_path, stage2_path, log)
+        except Exception as e:
+            log(f"Error when running GootLoaderAutoJsDecode due to {e}")
 
     if(urls and code):
         return GootLoaderConfig(code, urls)
