e5a14df660f9ba0f900c099a6f093ee03f26ca65
cccs-kevin <kevin.hardy-cooper@cyber.gc.ca>
2023-10-31 19:30:46 +0000

    Adding signature to catch reflective code-loading

--- signatures/manipulates_memory.py
+++ signatures/manipulates_memory.py
@@ -16,3 +16,20 @@ class MemoryStream(Signature):
 
     def process_output(self, output):
         self.check_indicators_in_list(output)
+
+
+class ReflectiveCodeLoading(Signature):
+    def __init__(self):
+        super().__init__(
+            heuristic_id=3,
+            name="reflective_code_loading",
+            description="JavaScript uses classes found in .NET Runtime Assembly",
+            indicators=["Deserialize_2(", "DynamicInvoke("],
+            severity=0,
+        )
+
+    def process_output(self, output):
+        indicator_list = [
+            {"method": "any", "indicators": self.indicators},
+        ]
+        self.check_multiple_indicators_in_list(output, indicator_list)
