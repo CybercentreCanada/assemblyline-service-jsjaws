eff4129fa941e0743bdb62bc98dc6f9453904906
CCCS-Omar <omar.siage@cyber.gc.ca>
2024-01-15 13:51:02 -0500

    Updated the run method to default on the Mandiant extractor if it ddoesn't retrieve any download links.

--- tools/gootloader/run.py
+++ tools/gootloader/run.py
@@ -6,6 +6,10 @@ from tools.gootloader.GootLoaderAutoJsDecode import gootDecode
 from tools.gootloader.utils import GootLoaderConfig
 
 
+class DownloadLinksNotExtracted(Exception):
+    pass
+
+
 def run(
     js_file_path: str, unsafe_uris: bool = False, payload_path: str = None, stage2_path: str = None, log: Logger = print
 ) -> Optional[GootLoaderConfig]:
@@ -18,13 +22,17 @@ def run(
         if third_generation:
             """The path of the second stage never changes"""
             _, urls, code = goot_decode_modified(stage2_path, unsafe_uris, payload_path, stage2_path, log)
+        if not urls:
+            raise DownloadLinksNotExtracted(
+                "Couldn't extract download links. Repeating extraction using Mandiant's script."
+            )
     except Exception as e:
         log(f"Falling back on GootLoaderAutoJsDecode due to {e}")
         """Fallback on the unmodified script"""
         try:
             third_generation, urls, code = gootDecode(js_file_path, unsafe_uris, payload_path, stage2_path, log)
             if third_generation:
-                print("calling again")
+                log("Calling again")
                 _, urls, code = gootDecode(stage2_path, unsafe_uris, payload_path, stage2_path, log)
         except Exception as e:
             log(f"Error when running GootLoaderAutoJsDecode due to {e}")
