0cf35d7059f18b69eb8bde669a5e9a13aefb1d4c
cccs-kevin <kevin.hardy-cooper@cyber.gc.ca>
2024-01-18 18:32:37 +0000

    Changing scores for split_reverse_join and dom_writes_equal_2; Refactoring writes_archive

--- signatures/save_to_file.py
+++ signatures/save_to_file.py
@@ -1,6 +1,7 @@
 """
 These are all of the signatures related to saving a file
 """
+from assemblyline.common.str_utils import safe_str
 from signatures.abstracts import ANY, Signature
 
 # List of commands used to save a file to disk
@@ -45,16 +46,33 @@ class WritesArchive(Signature):
             heuristic_id=3,
             name="writes_archive",
             description="JavaScript writes archive file to disk",
-            indicators=[".zip", ".iso"],
+            indicators=["\\.zip", "\\.iso"],
             severity=3,
         )
 
     def process_output(self, output):
-        indicator_list = [
-            {"method": ANY, "indicators": save_commands},
-            {"method": ANY, "indicators": self.indicators},
-        ]
-        self.check_multiple_indicators_in_list(output, indicator_list)
+        extension_results = []
+        results = []
+
+        # First look for file extensions
+        extension_regex = f"(?i)({'|'.join(self.indicators)})\\b"
+        for line in output:
+            split_line = line.split("] ")
+            if len(split_line) == 2:
+                string = split_line[1]
+            if self.check_regex(extension_regex, string.lower()):
+                extension_results.append(string)
+
+        # Next look for the command
+        escaped_save_commands = [save_command.replace("(", "\\(") for save_command in save_commands]
+        commands_regex = f"({'|'.join(escaped_save_commands)})"
+        for line in extension_results:
+            if self.check_regex(commands_regex, line):
+                results.append(line)
+
+        results_set = sorted(set(results))
+        for result in results_set:
+            self.marks.append(safe_str(result).strip())
 
 
 class CopyFile(Signature):
