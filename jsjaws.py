d2969b90359599bfa76e836a91aa9aca1847325f
cccs-kevin <kevin.hardy-cooper@cyber.gc.ca>
2024-03-01 13:33:56 +0000

    Adding regex check that form action is indeed a URL

--- jsjaws.py
+++ jsjaws.py
@@ -4427,7 +4427,7 @@ class JsJaws(ServiceBase):
                     if not value:
                         continue
                     # https://developer.mozilla.org/en-US/docs/Web/HTML/Element/form#attributes_for_form_submission
-                    if key == "action":
+                    if key == "action" and re.match(FULL_URI, value):
                         form_has_action = True
                         if self.single_script_with_unescape:
                             # A form with an action was created from a single script that used an unescape AND the form
@@ -4454,7 +4454,7 @@ class JsJaws(ServiceBase):
                                 if not value:
                                     continue
                                 # https://developer.mozilla.org/en-US/docs/Web/HTML/Element/form#action
-                                if key == "formaction":
+                                if key == "formaction" and re.match(FULL_URI, value):
                                     form_has_action = True
                                     if self.single_script_with_unescape:
                                         # A form with an action was created from a single script that used an
